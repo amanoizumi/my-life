14e32829916b9a901d481732aa660564f0a11394
amanoizumi <56188919+amanoizumi@users.noreply.github.com>
2025-05-15 08:09:17 +0000

    Add module preview

--- components/ModulePreview/ModulePreviewStandardFourImageAndText.vue
+++ components/ModulePreview/ModulePreviewStandardFourImageAndText.vue
@@ -1,3 +1,31 @@
 <template>
-  <div></div>
+  <div>
+    <h2 class="font-bold text-2xl mb-3">
+      {{ store.headline }}
+    </h2>
+    <ul class="grid grid-cols-4 gap-3">
+      <li v-for="item in store.list" :key="item.order">
+        <div class="card bg-base-100 shadow-sm h-full">
+          <figure>
+             
+            <img
+              class="w-full h-full aspect-square object-cover"
+              :src="item.imgUrl"
+              alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">{{ item.headline }}</h2>
+            <p>
+              {{ item.bodyText }}
+            </p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
+<script setup>
+import { useModuleTemplateStandardFourImageAndTextStore } from '~/stores/blockModules/moduleTemplateStandardFourImageAndText.js';
+
+const store = useModuleTemplateStandardFourImageAndTextStore();
+</script>
--- components/ModuleTemplate/ModuleTemplateStandardFourImageAndText.vue
+++ components/ModuleTemplate/ModuleTemplateStandardFourImageAndText.vue
@@ -16,7 +16,7 @@
           <input
             type="text"
             class="input"
-            v-model="moduleTemplateStandardFourImageAndTextStore.headline"
+            v-model="store.headline"
             id="standardFourImageAndTextTempalteInput"
             placeholder="Enter headline text" />
         </div>
@@ -24,14 +24,9 @@
     </div>
 
     <h3 class="text-lg font-medium">Images</h3>
-    <!-- <pre>headline: {{ headline }}</pre>
-    <pre>{{ list }}</pre> -->
+
     <ul class="grid grid-cols-4 gap-3" id="standardFourImageAndTextList">
-      <li
-        v-for="(
-          item, index
-        ) in moduleTemplateStandardFourImageAndTextStore.list"
-        :key="item.order">
+      <li v-for="(item, index) in store.list" :key="item.order">
         <div class="flex flex-col">
           <p class="text-lg">
             Image
@@ -39,17 +34,15 @@
           </p>
           <div>
             <div
-              v-if="!item.imgUrl"
+              v-if="!store.list[index].imgUrl"
               class="border-black border-2 border-dashed bg-orange-300 mb-3 flex justify-center items-center aspect-square cursor-pointer"
               role="button"
-              @click="
-                useModuleTemplateStandardFourImageAndTextStore.uploadImage
-              ">
+              @click="store.list[index].uploadImage">
               <p>Click to add image</p>
             </div>
             <div v-else class="border-transparent border-2 mb-3 aspect-square">
               <img
-                :src="item.imgUrl"
+                :src="store.list[index].imgUrl"
                 class="w-full h-full object-cover"
                 alt="" />
             </div>
@@ -59,7 +52,7 @@
               <h3 class="text-lg font-medium">Headline</h3>
             </label>
             <input
-              v-model="item.headline"
+              v-model="store.list[index].headline"
               type="text"
               placeholder="Type here"
               class="input" />
@@ -69,7 +62,7 @@
               <h3 class="text-lg font-medium">Body Text</h3>
             </label>
             <textarea
-              v-model="item.bodyText"
+              v-model="store.list[index].bodyText"
               type="text"
               class="textarea"
               id="bodyText1"
@@ -83,6 +76,5 @@
 <script setup>
 import { useModuleTemplateStandardFourImageAndTextStore } from '~/stores/blockModules/moduleTemplateStandardFourImageAndText.js';
 
-const moduleTemplateStandardFourImageAndTextStore =
-  useModuleTemplateStandardFourImageAndTextStore();
+const store = useModuleTemplateStandardFourImageAndTextStore();
 </script>
